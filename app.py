57d27055c372726052c3a0bc757dde6b3cc51508
Sanket Kapse <sanket8397@gmail.com>
2023-11-28 00:37:20 -0700

    Temporal API changes for frontend

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
+
 from summary import *
 from state_city import *
 from temporal import *
 from details import *
 
 app = Flask(__name__)
+CORS(app)
+
 
 @app.route('/api/hello', methods=['GET'])
 def hello():
@@ -15,35 +19,42 @@ def hello():
 # http://127.0.0.1:5000/api/summary/map?year=2015&year=2016&year=2017
 # http://127.0.0.1:5000/api/summary/map
 
+
 @app.route('/api/summary/map', methods=['GET'])
 def map_route():
     years_list = request.args.getlist('year')
     return jsonify(get_map_data(years_list))
+
 
 @app.route('/api/summary/gender', methods=['GET'])
 def gender_route():
     years_list = request.args.getlist('year')
     return jsonify(get_gender_data(years_list))
 
+
 @app.route('/api/summary/race', methods=['GET'])
 def race_route():
     years_list = request.args.getlist('year')
     return jsonify(get_race_data(years_list))
+
 
 @app.route('/api/summary/mannerofdeath', methods=['GET'])
 def manner_of_death_route():
     years_list = request.args.getlist('year')
     return jsonify(get_manner_of_death_data(years_list))
 
+
 @app.route('/api/summary/armedwith', methods=['GET'])
 def armed_with_route():
     years_list = request.args.getlist('year')
     return jsonify(get_armed_with_data(years_list))
 
+
 @app.route('/api/summary/fleeing', methods=['GET'])
 def fleeing_route():
     years_list = request.args.getlist('year')
     return jsonify(get_fleeing_data(years_list))
+
 
 @app.route('/api/summary/mentalillness', methods=['GET'])
 def mental_illness_route():
@@ -53,10 +64,13 @@ def mental_illness_route():
 ######################### Summary routes ############################
 
 ######################### State City routes ############################
+
+
 @app.route('/api/statecity/cities', methods=['GET'])
 def cities_route():
     state = request.args.get('state')
     return jsonify(get_cities(state))
+
 
 @app.route('/api/statecity/income', methods=['GET'])
 def cities_income_route():
@@ -64,11 +78,13 @@ def cities_income_route():
     cities = get_cities_original(state)
     return jsonify(get_median_income(cities))
 
+
 @app.route('/api/statecity/poverty', methods=['GET'])
 def cities_poverty_route():
     state = request.args.get('state')
     cities = get_cities_original(state)
     return jsonify(get_poverty_rate(cities))
+
 
 @app.route('/api/statecity/gradrate', methods=['GET'])
 def cities_highschool_grad_rate_route():
@@ -76,17 +92,20 @@ def cities_highschool_grad_rate_route():
     cities = get_cities_original(state)
     return jsonify(get_highschool_grad_rate(cities))
 
+
 @app.route('/api/statecity/cityvictimcount', methods=['GET'])
 def cities_count_route():
     state = request.args.get('state')
     cities = get_cities_original(state)
     return jsonify(get_cities_count_data(cities))
 
+
 @app.route('/api/statecity/cityrace', methods=['GET'])
 def cities_race_distribution_route():
     state = request.args.get('state')
     cities = get_cities_original(state)
     return jsonify(get_cities_race_data(cities))
+
 
 @app.route('/api/statecity/cityracevictimcount', methods=['GET'])
 def cities_race_count_route():
@@ -97,9 +116,12 @@ def cities_race_count_route():
 ######################### State City routes ############################
 
 ######################### Temporal Routes ############################
+
+
 @app.route('/api/temporal/count', methods=['GET'])
 def temporal_count_route():
     return jsonify(get_temporal_data())
+
 
 @app.route('/api/temporal/racecount', methods=['GET'])
 def temporal_race_count_route():
@@ -108,11 +130,14 @@ def temporal_race_count_route():
 ######################### Temporal Routes ############################
 
 ######################### Details Routes ############################
+
+
 @app.route('/api/details', methods=['GET'])
 def details_route():
     return jsonify(get_victim_details_data())
 
 ######################### Details Routes ############################
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- temporal.py
+++ temporal.py
@@ -1,36 +1,34 @@
 from query_generator import *
 from utils import *
 
+
 def get_temporal_data():
     query = get_year_count_query()
     results = execute_sparql(query)
 
-    victim_data = []
+    victim_data = {}
     for result in results["results"]["bindings"]:
         deathYear = result["deathYear"]["value"]
         victim_count = result["victimCount"]["value"]
-        year_victim_count = {}
-        year_victim_count["year"] = deathYear
-        year_victim_count["count"] = victim_count
-        victim_data.append(year_victim_count)
+        victim_data[deathYear] = int(victim_count)
 
     return victim_data
+
 
 def get_year_race_count_data():
     query = get_year_race_count_query()
     results = execute_sparql(query)
 
-    race_mapping = {"W": "White", "H": "Hispanic", "A": "Asian", "B": "Black", "N": "Native American", "O": "Other"}
+    race_mapping = {"W": "White", "H": "Hispanic", "A": "Asian",
+                    "B": "Black", "N": "Native American", "O": "Other"}
 
-    year_victim_data = []
+    victim_data = {}
     for result in results["results"]["bindings"]:
         deathYear = result["deathYear"]["value"]
-        race = result["race"]["value"]
+        race = race_mapping[result["race"]["value"]]
         victim_count = result["victimCount"]["value"]
-        year_victim_count = {}
-        year_victim_count["year"] = deathYear
-        year_victim_count["race"] = race_mapping[race]
-        year_victim_count["count"] = victim_count
-        year_victim_data.append(year_victim_count)
+        if race not in victim_data:
+            victim_data[race] = {}
+        victim_data[race][deathYear] = int(victim_count)
 
-    return year_victim_data
+    return victim_data
